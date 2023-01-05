f62d8063e9f8b83023c4eb9f5e8767e2e5952a38
CaiOliveira1991 <121823070+CaiOliveira1991@users.noreply.github.com>
2023-01-05 01:11:27 -0300

    Menu

--- src/Components/Burger.tsx
+++ src/Components/Burger.tsx
@@ -10,11 +10,6 @@ import {motion} from 'framer-motion'
 
 import {AiOutlineClose} from 'react-icons/ai'
 
-// Img
-
-import Cullinan from '../assets/culinan.jpg'
-import Spectre from '../assets/Spectre2023.jpg'
-
 const StyledBurger = styled.div`
     width: 2rem;
     height: 2rem;
@@ -64,13 +59,13 @@ const StyledBurger = styled.div`
 
 const Containershow = styled.div`
   position: fixed;
-  top: 0;
-  left: -5%;
-  width: 40%;
-  min-width: 130px;
-  height: 100vh;
+  top: 12vh;
+  left: 0;
+  width: 30%;
+  min-width: 200px;
+  height: 500px;
   background-color: #a724a7;
-  border-radius: 0px 30px 30px 0px;
+  border-radius: 30px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -78,50 +73,13 @@ const Containershow = styled.div`
   z-index: 999;
 `;
 
-const ContainershowRoom = styled.div`
-  position: fixed;
-  margin-left: 5%;
-  top: 0;
-  width: 30%;
-  min-width: 200px;
-  height: 100vh;
-  border-radius: 0px 30px 30px 0px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  z-index: 998;
-`;
-
 const Close = styled.div`
   width: 100%;
   height: 12vh;
   top: 0;
   left: 0;
-  border-radius: 0px 30px 0px 0px;
+  border-radius: 30px 30px 0px 0px;
   background-color: #571186;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  button{
-    padding: 10px;
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    }
-`;
-
-const CloseRoom = styled.div`
-  width: 100%;
-  height: 12vh;
-  top: 0;
-  left: 0;
-  border-radius: 0px 30px 0px 0px;
-  background-color: #571150;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -152,37 +110,6 @@ const ShowRoom = styled.div`
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    p{
-      width: 100%;
-      height: 50px;
-      text-transform: uppercase;
-      cursor: pointer;
-      color: white;
-      font-size: 1.5rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 30px;
-      z-index: 999;
-    }
-  }
-`;
-
-const ShowRoomm = styled.div`
-  mergin-top: 12vh;
-  width: 100%;
-  height: 88%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-  div{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
   }
 `;
 
@@ -199,41 +126,9 @@ const NvLink = styled(Link)`
   z-index: 999;
 `;
 
-const NvShow = styled(Link)`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  :nth-child(1){
-    background-image: url(${Cullinan});
-    background-position: center;
-    background-size: cover;
-  }
-  :nth-child(2){
-    background-image: url(${Spectre});
-    background-position: center;
-    background-size: cover;
-  }
-  &:hover span{
-    transform: scale(1.3);
-    color: #02b9f6;
-    text-shadow: 0px 0px 8px #02b9f6;
-    transition: 0.2s ease;
-  }
-  span{
-    font-size: 30px;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
 const Burger = () => {
 
   const [ clickmenu , setClickmenu ] = useState (false);
-  const [ clickshow , setClickshow ] = useState (false);
 
   return (
 
@@ -249,13 +144,17 @@ const Burger = () => {
 {clickmenu && (
 
 
-      <Containershow
-       as={motion.div}  
-       initial={{ opacity: 0, x: '-350px' }}
-       animate={{ opacity: 1, x: '0px' }}
-       transition={{
-         duration: 0.3,
-         easeInOut: [0, 0.71, 0.2, 1.01]
+    <Containershow
+      as={motion.div}  drag
+      dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
+      dragElastic={0.5}
+      whileTap={{ cursor: "grabbing" }}
+      initial={{ opacity: 0, scale: 0.2 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.3,
+        easeInOut: [0, 0.71, 0.2, 1.01]
      }}
      >
 
@@ -265,8 +164,7 @@ const Burger = () => {
           whileHover= {{ rotate: 180, borderColor: 'white' }}
           transition={{ duration: 0.2 }}
           >
-            <AiOutlineClose color={'white'} 
-            onClick={() => setClickshow (false)}/>
+            <AiOutlineClose color={'white'} />
           </motion.button>
         </Close>
 
@@ -274,6 +172,7 @@ const Burger = () => {
           <ul>
 
             <NvLink to='/' as={motion(Link)}
+              onClick={() => setClickmenu (false)}
               whileHover = {{
                 scale: 1.5,
                 textShadow: '0px 0px 8px #02b9f6',
@@ -283,9 +182,9 @@ const Burger = () => {
                 type: 'spring',
                 stiffness: 300
              }}
-             onClick={() => setClickshow (false)}
             >Home</NvLink>
-            <motion.p
+            <NvLink to='Show' as={motion(Link)}
+              onClick={() => setClickmenu (false)}
               whileHover = {{
                 scale: 1.5,
                 textShadow: '0px 0px 8px #02b9f6',
@@ -295,9 +194,9 @@ const Burger = () => {
                 type: 'spring',
                 stiffness: 300
              }}
-             onClick={() => setClickshow (true)}
-            >ShowRoom</motion.p>
+            >ShowRoom</NvLink>
             <NvLink to='Map' as={motion(Link)}
+              onClick={() => setClickmenu (false)}
               whileHover = {{
                 scale: 1.5,
                 textShadow: '0px 0px 8px #02b9f6',
@@ -308,56 +207,12 @@ const Burger = () => {
                 type: 'spring',
                 stiffness: 300
              }}
-             onClick={() => setClickshow (false)}
             >Localização</NvLink>
             
           </ul>
         </ShowRoom>
 
       </Containershow>
-
-      )}
-
-{clickshow && (
-
-      <ContainershowRoom
-        as={motion.div}  
-        initial={{ opacity: 0, x: '0' }}
-        animate={{ opacity: 1, x: '350px' }}
-        transition={{
-          duration: 0.3,
-          easeInOut: [0, 0.71, 0.2, 1.01]
-        }}
-      >
-
-        <CloseRoom>
-          <motion.button 
-          onClick={() => setClickshow (false)}
-          whileHover= {{ rotate: 180, borderColor: 'white' }}
-          transition={{ duration: 0.2 }}
-          >
-            <AiOutlineClose color={'white'} />
-          </motion.button>
-        </CloseRoom>
-
-        <ShowRoomm>
-          <div>
-
-            <NvShow to='/Cullinan/ApresentationCullinan'
-            onClick={() => setClickshow (false)}>
-              <span>Cullinan</span>
-            </NvShow>
-            
-            <NvShow to='/Spectre/ApresentationSpectre' as={motion(Link)}
-                        onClick={() => setClickshow (false)}>
-              <span>Spectre</span>
-          </NvShow>
-            
-          </div>
-        </ShowRoomm>
-        
-
-      </ContainershowRoom>
 
       )}
     </>
